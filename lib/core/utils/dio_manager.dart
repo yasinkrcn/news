871d6f3d56597ce2a63720a36c70768a782610fd
yasinkrcn <23yasin1999@gmail.com>
2022-10-11 21:56:38 +0300

    new api keys added

--- lib/core/utils/dio_manager.dart
+++ lib/core/utils/dio_manager.dart
@@ -6,13 +6,15 @@ class DioManager {
 
     dio.options.baseUrl = 'https://newsapi.org/';
     dio.options.queryParameters = {
-      'apiKey': '1a6b35171765474db053de666f2fd1ca',
+      'apiKey': 'd81af8ec02164ca28a61176abdf65fc5',
       // 'apiKey': 'aecf1d77da4342518f249c8e7c692100',
       // 'apiKey': 'bcccb776f37e4a699c36ce0a4939c95f',
       // 'apiKey': 'ecb65575edc449419a70612b6e093a2d',
       // 'apiKey': '1a6b35171765474db053de666f2fd1ca',
       // 'apiKey': 'd9df1b8914104f0ca093ba5c5817bb97',
       // 'apiKey': '78fc3e6fb7164695afb059af02d801ab',
+      // 'apiKey': 'd64e95b7749f42c9a5a4a5a7fb05747d',
+      // 'apiKey': 'd81af8ec02164ca28a61176abdf65fc5',
     };
     dio.options.headers = {
       'Accept': 'application/json',
@@ -21,11 +23,10 @@ class DioManager {
 
     dio.options.responseType = ResponseType.plain;
 
-    TODO: //dartz tipine uygun olarak yazılacak
+
 
     return dio;
   }
 }
 
 
-// https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=aecf1d77da4342518f249c8e7c692100
